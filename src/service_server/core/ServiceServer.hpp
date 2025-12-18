621dd802f94094464bf0594c89ff90dd1b22445f
otimeotime <otime33333@gmail.com>
2025-12-18 18:06:34 +0700

    Add ServiceServer files (core, database, logic)/ Add FacadePattern in PacketUtils

---
+++ src/service_server/core/ServiceServer.hpp
@@ -0,0 +1,30 @@
+#ifndef SERVICE_SERVER_H
+#define SERVICE_SERVER_H
+
+#include <vector>
+#include <thread>
+#include <atomic>
+#include "../../common/network/TCPSocket.hpp"
+#include "../logic/AuthServer.hpp" 
+
+class ServiceServer {
+private:
+    TCPSocket mServerSocket;
+    std::atomic<bool> mIsRunning;
+    
+    AuthServer mAuthServer;
+
+    void HandleClient(TCPSocket* clientSocket);
+
+public:
+    ServiceServer();
+    ~ServiceServer();
+
+    // The main loop that waits for connections
+    void Run(int port);
+    
+    // Signal to stop the server
+    void Stop();
+};
+
+#endif
