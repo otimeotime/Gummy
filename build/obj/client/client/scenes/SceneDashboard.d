3f2f9e18a0663eaa45b52e7f952552669f98ef58
NQS04 <son.nq225998@sis.hust.edu.vn>
2026-01-09 15:38:16 +0700

    Merge integration into feature/advanced

---
+++ build/obj/client/client/scenes/SceneDashboard.d
@@ -0,0 +1,55 @@
+build/obj/client/client/scenes/SceneDashboard.o: \
+ src/client/scenes/SceneDashboard.cpp \
+ src/client/scenes/SceneDashboard.hpp \
+ src/client/scenes/../core/GameState.hpp \
+ src/client/scenes/../ui/UIObject.hpp \
+ src/client/scenes/../ui/../../common/gui/Vector2D.hpp \
+ src/client/scenes/../ui/Text.hpp src/client/scenes/../ui/Button.hpp \
+ src/client/scenes/../ui/TextInput.hpp \
+ src/client/scenes/../network/ClientSocket.hpp \
+ src/client/scenes/../network/../../common/network/TCPSocket.hpp \
+ src/client/scenes/../network/../../common/network/Packet.hpp \
+ src/client/scenes/../network/../../common/network/PacketType.hpp \
+ src/client/scenes/../network/../../common/network/PacketType.hpp \
+ src/client/scenes/../network/../../common/network/PacketStructs.hpp \
+ src/client/scenes/../network/../../common/network/PacketUtils.hpp \
+ src/client/scenes/../network/../../common/network/Packet.hpp \
+ src/client/scenes/../network/../../common/network/TCPSocket.hpp \
+ src/client/scenes/SceneLogin.hpp src/client/scenes/SceneGame.hpp \
+ src/client/scenes/../core/Game.hpp src/client/scenes/../core/Window.hpp \
+ src/client/scenes/../core/StateMachine.hpp \
+ src/client/scenes/../core/InputHandler.hpp \
+ src/client/scenes/../core/TextureManager.hpp \
+ src/client/scenes/../../common/network/TCPSocket.hpp \
+ src/client/scenes/../../common/network/PacketUtils.hpp \
+ src/client/scenes/../../common/network/PacketStructs.hpp \
+ src/client/scenes/../../ingame_server/logic/MapLoader.hpp \
+ src/client/scenes/SceneViewProfile.hpp
+src/client/scenes/SceneDashboard.hpp:
+src/client/scenes/../core/GameState.hpp:
+src/client/scenes/../ui/UIObject.hpp:
+src/client/scenes/../ui/../../common/gui/Vector2D.hpp:
+src/client/scenes/../ui/Text.hpp:
+src/client/scenes/../ui/Button.hpp:
+src/client/scenes/../ui/TextInput.hpp:
+src/client/scenes/../network/ClientSocket.hpp:
+src/client/scenes/../network/../../common/network/TCPSocket.hpp:
+src/client/scenes/../network/../../common/network/Packet.hpp:
+src/client/scenes/../network/../../common/network/PacketType.hpp:
+src/client/scenes/../network/../../common/network/PacketType.hpp:
+src/client/scenes/../network/../../common/network/PacketStructs.hpp:
+src/client/scenes/../network/../../common/network/PacketUtils.hpp:
+src/client/scenes/../network/../../common/network/Packet.hpp:
+src/client/scenes/../network/../../common/network/TCPSocket.hpp:
+src/client/scenes/SceneLogin.hpp:
+src/client/scenes/SceneGame.hpp:
+src/client/scenes/../core/Game.hpp:
+src/client/scenes/../core/Window.hpp:
+src/client/scenes/../core/StateMachine.hpp:
+src/client/scenes/../core/InputHandler.hpp:
+src/client/scenes/../core/TextureManager.hpp:
+src/client/scenes/../../common/network/TCPSocket.hpp:
+src/client/scenes/../../common/network/PacketUtils.hpp:
+src/client/scenes/../../common/network/PacketStructs.hpp:
+src/client/scenes/../../ingame_server/logic/MapLoader.hpp:
+src/client/scenes/SceneViewProfile.hpp:
